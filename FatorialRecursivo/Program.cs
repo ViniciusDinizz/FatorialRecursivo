cce96075a5c89cf18e5520f188fe4b1ddb433d5d
ViniciusDinizz <viniciusdiniz.ti/2gmail.com>
2023-03-06 22:59:36 -0300

    Add project files.

---
+++ FatorialRecursivo/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int fatori = 0;
+
+        Console.Write("Digite o número: ");
+        fatori = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"O fatorial de {fatori} = {funcFat(fatori, fatori)}");
+
+        int funcFat(int fatori, int value)
+        {
+            if (fatori == 1)
+            {
+                return value;
+            }
+            else
+            {
+                return funcFat(fatori - 1, value *= fatori - 1);
+            }
+        }
+    }
+}
